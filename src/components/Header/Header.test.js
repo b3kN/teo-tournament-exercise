f033f2e7600e064799881c3c1ba35649ee3b3bfb
Nicholas Bekeris <nicholas@b3kndev.com>
2019-01-16 22:40:06 -0600

    Implemented complete Header unit tests

---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Header from './';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Header Component', () => {
+	const header = shallow(<Header title="Tournament Exercise" />);
+
+	it('renders with default title', () => {
+		expect(header.find('h1').text().trim()).toEqual("Tournament Exercise");
+	});
+
+	it('renders with standard sub-title', () => {
+		expect(header.find('.subheader').text().trim()).toEqual("Written by @b3kN");
+	});
+});
