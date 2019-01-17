448df3e31f8ee8f23c97af900300a0dce576de1d
Nicholas Bekeris <nicholas@b3kndev.com>
2019-01-16 22:50:31 -0600

    Implemented Enzyme with Application unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, { mount, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
+Enzyme.configure({ adapter: new Adapter() });
+
 describe('Application', () => {
+	let app;
+
 	it('renders without crashing', () => {
-		const div = document.createElement('div');
-		ReactDOM.render(<App />, div);
+		shallow(<App />);
+	});
+
+	it('creates holder div for tournament information cards', () => {
+		app = mount(<App />);
+
+		expect(app.find('.tournaments-holder').length).toEqual(1);
 	});
 });
