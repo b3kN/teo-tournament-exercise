5a547a161e47010296ebd42c4989d9ab0754a8d8
Nicholas Bekeris <nicholas@b3kndev.com>
2019-01-16 22:56:20 -0600

    Updated implementation of Tournament unit tests

--- src/components/Tournament/Tournament.test.js
+++ src/components/Tournament/Tournament.test.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, { mount, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import Tournament from './';
 
+Enzyme.configure({ adapter: new Adapter() });
+
 describe('Tournament Component', () => {
+	let tournament;
+
 	it('renders without crashing', () => {
-		const tournament = document.createElement('div');
-		ReactDOM.render(<Tournament id="00000" />, tournament);
+		shallow(<Tournament id="test" />);
+	});
+
+	it('creates holder div for tournament header information', () => {
+		tournament = mount(<Tournament id="test" />);
+
+		expect(tournament.find('.header-holder').length).toEqual(1);
+	});
+
+	it('header holder contains default title string', () => {
+		tournament = mount(<Tournament id="test" />);
+
+		expect(tournament.find('.header-holder').text()).toEqual("Tournament Data Loading...");
 	});
 });
