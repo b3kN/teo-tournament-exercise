43037f459b50b8df85098dc2ad22d3c91a455077
Nicholas Bekeris <nicholas@b3kndev.com>
2019-01-17 22:08:33 -0600

    Implementation of results and contestants gathering

--- src/components/Matches/index.js
+++ src/components/Matches/index.js
@@ -5,33 +5,89 @@ class Matches extends Component {
 		super(props);
 
 		this.state = {
+			loading: true,
 			id: props.league,
-			contestants: [],
-			results: []
+			results: null,
+			contestants: null
 		};
 	}
 
 	componentDidMount() {
-		var corsBypass = "https://cors-anywhere.herokuapp.com/";
+		this.getTournamentResults();
+	}
 
-		fetch(corsBypass + `https://api.eslgaming.com/play/v1/leagues/${this.state.id}/results`)
-			.then(response => response.json())
-			.then(data => this.setState({ results: data }))
-			.catch(e => console.log('error', e));
+	getTournamentResults() {
+		const corsBypass = "https://cors-anywhere.herokuapp.com/";
 
 		fetch(corsBypass + `https://api.eslgaming.com/play/v1/leagues/${this.state.id}/contestants`)
 			.then(response => response.json())
-			.then(data => this.setState({ contestants: data }))
-			.catch(e => console.log('error', e));
+			.then(teams => this.setState({ contestants: teams }))
+			.then(() => {
+				fetch(corsBypass + `https://api.eslgaming.com/play/v1/leagues/${this.state.id}/results`)
+					.then(response => response.json())
+					.then(data => {
+						let tempResults = [];
+
+						data.forEach(item => {
+							let result = new Result(item.beginAt, [], item.state);
+
+							// console.log('Create new result item for current item', result);
+
+							item.participants.forEach(p => {
+								let team = this.state.contestants.find(c => c.id === p.id);
+
+								if (team) {
+									let participant = new Participant(team.name, p.points[0], p.place);
+
+									// console.log('Add participant to current result item', participant);
+
+									result.participants.push(participant);
+								}
+							});
+
+							tempResults.push(result);
+						});
+
+						return tempResults;
+					}).then(finalResults => {
+						console.log('finalResults', finalResults);
+
+						if (finalResults.length > 0) {
+							finalResults.sort(function (a, b) {
+								return new Date(a.date) - new Date(b.date);
+							});
+
+							this.setState({ results: finalResults });
+						}
+
+						// console.log('Final matches list:', this.state.results);
+					});
+			}).catch(e => console.log('Error while fetching league results', e));
 	}
 
 	render() {
 		return (
-			<div className="matches-holder">
-				Lorem Ipsum...
+			<div className="testing">
+				TESTING...
 			</div>
 		);
 	}
 }
 
+class Result {
+	constructor(date, participants, state) {
+		this.date = date;
+		this.participants = participants;
+		this.state = state;
+	}
+}
+
+class Participant {
+	constructor(name, score, place) {
+		this.name = name;
+		this.score = score;
+		this.place = score;
+	}
+}
+	
 export default Matches;
